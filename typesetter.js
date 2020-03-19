381a6371cff6a54bdbd8bbf58e9caee38723352a
Tom Bubul <tom.bubul@gmail.com>
2020-03-19 15:52:47 -0400

    overhauled letterset, small cleanup

--- typesetter.js
+++ typesetter.js
@@ -14,48 +14,7 @@ function Print () {
 
     var newCharacter = document.createElement("li"); 
     var characterValue = printable[charCount].toLowerCase()
-    /* not sure we need any of this
-    var textnode = document.createTextNode( withValue );
-    character.appendChild(textnode);
-    */
-
-    // newCharacter.setAttribute("id", characterValue )
-    // newCharacter.setAttribute("style", "background-image: url('" + characterValue + ".png');" )
-
     var div = document.getElementById('word')
-    div.insertAdjacentHTML('beforebegin', "<img src=" + characterValue + ".png>");
-
-    // document.getElementById("word").appendChild(newCharacter);
-
-    // SetCharacterSize( printable[charCount] , charCount )
+    div.insertAdjacentHTML('beforebegin', "<img src=letterset2/" + characterValue + ".png>");
   }
 }
-
-/* 
-  - get the background image dimensions for a character
-  - extract that image's dimensions
-  - update the char a min-width/min-height
-  */
-
-// function SetCharacterSize ( char , offsetMultiplier ) {
-
-//     var imageSrc = document.getElementById( char )
-//       .style
-//       .backgroundImage
-//       .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-//       .split(',')[0];
-
-//     console.log("image source = " + imageSrc)
-
-//     var image = new Image()
-//     image.src = imageSrc
-
-//     var setSizes = document.getElementById ( char )
-
-//     var marginOffset = -60
-//     // var marginOffset = offsetMultiplier * image.width * -0.10;
-
-//     // this is wrong/racey:
-
-//     setSizes.setAttribute("style", "background-image: url('" +image.src+ "');min-width:"+image.width+";min-height:"+image.height+";z-index:"+offsetMultiplier+";margin-left:"+marginOffset+";" )
-// }
